51cdf5d8d1c819df7eaf68faa50fccd6191059fc
karangejo <gabriel.g.gras@gmail.com>
2021-03-31 14:59:40 +0800

    release

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -23,7 +23,7 @@ config :taiwan_buoys, TaiwanBuoysWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :taiwan_buoys, TaiwanBuoysWeb.Endpoint, server: true
+config :taiwan_buoys, TaiwanBuoysWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
